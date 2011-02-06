4e93ad649a4df8d2916752397913174886d495af
buivantienduc <buivantienduc@gmail.com>
2011-02-06 18:00:59 +0000

    ""

--- application/models/Users.model.php
+++ application/models/Users.model.php
@@ -1,9 +1,14 @@
 <?php
 
-class Users extends DbAbstraction 
+class Users extends Model 
 {
 	protected $_table_name = TB_USERS;
 	protected $_primary_key = 'user_id';
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}	
 }
 
 ?>
