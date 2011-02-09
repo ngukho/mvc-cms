7918f939c96a2c11527136c0034d85fccdba67cb
buivantienduc@gmail.com <buivantienduc@gmail.com@674b0cd9-5cea-49f6-62ae-3f674a232d40>
2011-02-09 04:09:49 +0000

    ""

--- trunk/lib/core/View.class.php
+++ trunk/lib/core/View.class.php
@@ -3,11 +3,6 @@
  *
  * @View Class
  *
- * @copyright Copyright (C) 2009 PHPRO.ORG. All rights reserved.
- *
- * @license new bsd http://www.opensource.org/licenses/bsd-license.php
- * @package Core
- *
  */
 
 class View
