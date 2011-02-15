495a1e22be06a8b5547ee97fd4ff8fb67c5fa67b
buivantienduc@gmail.com <buivantienduc@gmail.com@674b0cd9-5cea-49f6-62ae-3f674a232d40>
2011-02-15 04:33:55 +0000

    ""

--- trunk/application/controllers/site/IndexController.php
+++ trunk/application/controllers/site/IndexController.php
@@ -10,6 +10,8 @@ class Site_IndexController extends BaseController
 
 	public function indexAction() 
 	{
+//		$_SESSION['test'] = 12;
+		$this->oSession->userdata['test'] = 12;
 	    $this->_view->title = 'Welcome to Bui Van Tien Duc MVC';
 	    $this->renderView('site/home/index');
 	}
