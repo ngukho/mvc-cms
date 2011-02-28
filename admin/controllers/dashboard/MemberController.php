66585f065e56d96c6d0841252cff5bb46ac2d232
buivantienduc <buivantienduc@gmail.com>
2011-02-28 10:43:24 +0000

    ""

--- admin/controllers/dashboard/MemberController.php
+++ admin/controllers/dashboard/MemberController.php
@@ -15,7 +15,25 @@ class Dashboard_MemberController extends AdminController
 	
 	public function loginAction() 
 	{
+		if(isset($_POST))
+		{
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			
+			$users = new Users();
+			$row = $users->checkLogin($username, $password);
+			
+			if($row)
+			{
+				$this->oSession->userdata['current_admin'] = $row;
+				$this->oSession->userdata['is_logged'] = TRUE;
+				redirect('dashboard/panel/show');
+			}
+		}
+		
 		$this->oResponse->setOutput($this->_view->fetch('dashboard/member/login'), $this->oConfig->config_values['application']['config_compression']);
 	}
+	
+	
 
 }
