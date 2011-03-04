d3fdc52f097c07290a9b7facba5584d6f6fe90f7
buivantienduc <buivantienduc@gmail.com>
2011-03-04 10:34:46 +0000

    ""

--- index.php
+++ index.php
@@ -21,91 +21,10 @@ try
 	define ('__LAYOUT_PATH', __SITE_PATH.'/layouts');
 	define ('__HELPER_PATH', __APP_PATH.'/helpers');
 	
-	require __SITE_PATH . '/lib/core/FrontController.class.php';
-	require __SITE_PATH . '/lib/core/IController.class.php';
-	require __SITE_PATH . '/lib/core/BaseController.class.php';
-	require __SITE_PATH . '/lib/core/Request.class.php';
-	require __SITE_PATH . '/lib/core/Response.class.php';
-	require __SITE_PATH . '/lib/core/View.class.php';
-	require __SITE_PATH . '/lib/core/Registry.class.php';
-	require __SITE_PATH . '/lib/core/Config.class.php';
-	require __SITE_PATH . '/lib/core/Model.class.php';
-	require __SITE_PATH . '/lib/core/MvcException.class.php';
+	/*** include the helper ***/
+ 	$_autoload_helpers = array();
 	
-	// Load config files
-	require __APP_PATH . '/config/constants.php';
-	
- 	/*** include the helper ***/
- 	$autoload_helpers = array('common');
- 	
- 	/*** registry auto load ***/
-	spl_autoload_register(null, FALSE);
-	spl_autoload_extensions('.php, .class.php, .lang.php, .model.php');
-
-	// model loader
-	function modelLoader($class)
-	{
-		$filename = $class . '.model.php';
-		$file = __APP_PATH . "/models/$filename";
-		if (file_exists($file) == FALSE)
-		{
-			return FALSE;
-		}
-		include_once $file;
-	}
-
-	// autoload libs
-	function libLoader($class)
-	{
-		$filename = $class . '.class.php';
-		$file = __SITE_PATH . '/lib/' . $filename;
-		if (file_exists($file) == FALSE)
-		{
-			return FALSE;
-		}
-		include_once $file;
-	}
-	
-	spl_autoload_register('libLoader');
-	spl_autoload_register('modelLoader');
-	
-	// Load helper function
-	function helperLoader($functions)
-	{
-		if(!is_array($functions))
-			$functions = array($functions);
-			
-		foreach ($functions as $function)
-		{
-			$file_path = __HELPER_PATH . "/{$function}.helper.php";
-			if(file_exists($file_path))
-				include_once $file_path;
-		}			
-	}
-	helperLoader($autoload_helpers);
-	/* ---------------------------------------------------------- */
-	
-	// Load language  
-	$config = Config::getInstance();
-	$lang = $config->config_values['application']['language'];
-	$filename = strtolower($lang) . '.lang.php';
-	$file = __APP_PATH . '/lang/' . $filename;
-	include $file;
-
-	if (!function_exists('class_alias')) {
-	    function class_alias($original, $alias) {
-	        eval('abstract class ' . $alias . ' extends ' . $original . ' {}');
-	    }
-	}
-	// alias the lang class
-	class_alias($lang,'Lang');
-	/* -------------- */
-	
-	// set the timezone
-//	date_default_timezone_set($config->config_values['application']['timezone']);
-
-	/*** set error handler level to E_WARNING ***/
-	set_error_handler('_exception_handler', $config->config_values['application']['error_reporting']);
+	require __SITE_PATH . '/startup.php';
 	
  	/*** a new registry object ***/
  	$registry = new Registry();
