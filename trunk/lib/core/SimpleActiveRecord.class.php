a00029fb030490e68f7661b01bb80b54e15731e1
buivantienduc@gmail.com <buivantienduc@gmail.com@674b0cd9-5cea-49f6-62ae-3f674a232d40>
2011-03-06 18:20:58 +0000

    ""

--- trunk/lib/core/SimpleActiveRecord.class.php
+++ trunk/lib/core/SimpleActiveRecord.class.php
@@ -1,28 +1,11 @@
 <?php
 /*
-
 SimpleActiveRecord - A simple ORM classes for > PHP 5.2
-Copyright (C) 2010 Håkon Nessjøen <haakon.nessjoen@gmail.com>
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
 http://lunatic.no/2010/08/simple-orm-php-library-for-mysql/
-
 */
 
 class InvalidDbAdapterException extends Exception {}
+
 class SqlErrorException extends Exception {}
 
 // Define defaults for SimpleDbAdapters
