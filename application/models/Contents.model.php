4890cf29a7628ad1fe5243f635d01289f5e19147
buivantienduc <buivantienduc@gmail.com>
2011-03-09 04:58:52 +0000

    ""

--- application/models/Contents.model.php
+++ application/models/Contents.model.php
@@ -3,12 +3,20 @@
 class Contents extends SimpleActiveRecord
 {
 //	public $has_many = array( 'blogposts' => 'Blogpost' );
-//	public $belongs_to = array( 'customer' => 'Customer' );
+
 //	public $serialize = 'meta';
 
 	protected $primaryKey = 'id';
 	protected $tableName = TB_CONTENTS;
 
+//	public $has_many = array( 'oContents' => 'Contents:cat_id' );	
+//	public $belongs_to = array( 'oContentCat' => 'ContentCats:cat_id' );
+	public $belongs_to = array( 
+		'oContentCat' => array(
+			'columns' => 'cat_id' ,
+			'refTableClass' => 'ContentCats:id')
+	);
+	
 //	public function __construct()
 //	{
 //		parent::__construct();
