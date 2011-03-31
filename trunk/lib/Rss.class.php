3c32e9db3ae1c183ee3d51bfe4464c4fe85f6ecc
buivantienduc@gmail.com <buivantienduc@gmail.com@674b0cd9-5cea-49f6-62ae-3f674a232d40>
2011-03-31 03:24:05 +0000

    ""

--- trunk/lib/Rss.class.php
+++ trunk/lib/Rss.class.php
@@ -4,12 +4,45 @@
  *
  * A class to create RSS feeds using DOM
  *
- *
- * @copyright 2009
- *
- * @license BSD
- *
  */
+
+/* @example usage
+// the first article item 
+$item1 = array(
+	'title'=>'Zend Framework Example Site', 
+	'link'=>'http://www.phpro.org/articles/Zend-Framework-Example-Site.html', 
+	'description'=>'This example site hopes to introduce the newcomers to Zend Framework in a friendly way, by providing a simple modular site layout and can have the newcomer up and running in minutes.', 
+	'pubDate'=>date(DATE_RSS),
+	'image'=>array('link'=>'http://phpro.org', 'url'=>'http://phpro.org/images/spork.jpg', 'title'=>'SPORK'),
+	'language'=>'en');
+
+// second article item 
+$item2 = array(
+	'title'=>'Xajax-In An Object Oriented Environment',
+	'link'=>'http://www.phpro.org/tutorials/Xajax-In-An-Object-Oriented-Environment.html',
+	'description'=>'This tutorial takes the next step in development and shows, by way of example, how xajax can be utilized in an Object Oriented environment',
+	'pubDate'=>date(DATE_RSS),
+	'language'=>'en');
+
+// third article item 
+$item3 = array(
+	'title'=>'Introduction to SPL DirectoryIterator',
+	'link'=>'http://www.phpro.org/tutorials/Introduction-to-SPL-DirectoryIterator.html',
+	'description'=>'The DirectoryIterator is one of the more oft used of the Iterator classes and this tutorial helps to expose the user to developing in a standardised and Object Oriented approach',
+	'pubDate'=>date(DATE_RSS),
+	'language'=>'en');
+
+// a new RSS instance, pass values to the constructor 
+$rss = new Rss('PHPRO.ORG', 'http://phpro.org', 'PHP Articles Tutorials Examples Classes');
+
+// add the items from above 
+$rss->addItem($item1)
+	->addItem($item2)
+	->addItem($item3);
+
+// show the RSS Feed 
+echo $rss;
+*/
 
 class Rss extends DomDocument
 {
@@ -127,41 +160,5 @@ class Rss extends DomDocument
 		return $this->saveXML();
 	}
 }
-
-/*** the first article item ***/
-$item1 = array(
-	'title'=>'Zend Framework Example Site', 
-	'link'=>'http://www.phpro.org/articles/Zend-Framework-Example-Site.html', 
-	'description'=>'This example site hopes to introduce the newcomers to Zend Framework in a friendly way, by providing a simple modular site layout and can have the newcomer up and running in minutes.', 
-	'pubDate'=>date(DATE_RSS),
-	'image'=>array('link'=>'http://phpro.org', 'url'=>'http://phpro.org/images/spork.jpg', 'title'=>'SPORK'),
-	'language'=>'en');
-
-/*** second article item ***/
-$item2 = array(
-	'title'=>'Xajax-In An Object Oriented Environment',
-	'link'=>'http://www.phpro.org/tutorials/Xajax-In-An-Object-Oriented-Environment.html',
-	'description'=>'This tutorial takes the next step in development and shows, by way of example, how xajax can be utilized in an Object Oriented environment',
-	'pubDate'=>date(DATE_RSS),
-	'language'=>'en');
-
-/*** third article item ***/
-$item3 = array(
-	'title'=>'Introduction to SPL DirectoryIterator',
-	'link'=>'http://www.phpro.org/tutorials/Introduction-to-SPL-DirectoryIterator.html',
-	'description'=>'The DirectoryIterator is one of the more oft used of the Iterator classes and this tutorial helps to expose the user to developing in a standardised and Object Oriented approach',
-	'pubDate'=>date(DATE_RSS),
-	'language'=>'en');
-
-/*** a new RSS instance, pass values to the constructor ***/
-$rss = new rss('PHPRO.ORG', 'http://phpro.org', 'PHP Articles Tutorials Examples Classes');
-
-/*** add the items from above ***/
-$rss	->addItem($item1)
-	->addItem($item2)
-	->addItem($item3);
-
-/*** show the RSS Feed ***/
-echo $rss;
 
 ?>
